b79a2f23605c61c2401273204666730c2803c400
shimverz-app <shimverz@gmail.com>
2025-01-02 14:15:47 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.html("<canvas id='tutorial' width='100%'>오류중학교 정보화기기 수리 요청 대장</canvas>")
+
