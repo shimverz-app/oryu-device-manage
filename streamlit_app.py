ffd51276241daf595bccf65b2c7bbece7cc01d60
shimverz-app <shimverz@gmail.com>
2025-01-02 22:15:09 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,6 @@ import datetime as dt
 from st_aggrid import AgGrid, GridOptionsBuilder
 
 st.markdown("<span style='font-size: 23px;'>오류중학교 정보화 기기 수리 요청 대장</span>", unsafe_allow_html=True)
-data = pd.DataFrame([{'make_date': '11'}])
 
-gb = GridOptionsBuilder()
-gb.configure_default_column(
-    resizable=True,
-    filterable=True,
-    sortable=True,
-    editable=True,
-)
-gb.configure_column(field="make_date", header_name="요청 날짜", width=80)
-go = gb.build()
-grid_return = AgGrid(data, gridOptions=go)
+df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
+AgGrid(df)
