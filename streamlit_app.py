929031cc391a27c2f383d4659122827c74a1622a
shimverz-app <shimverz@gmail.com>
2025-01-02 14:22:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.html("<canvas id='myCanvas' width='100%'></canvas>")
+st.html("<canvas id='myCanvas' width='100%' height="100px"></canvas>")
 
 st.html("<script> const canvas = document.getElementById('myCanvas'); const ctx = canvas.getContext('2d'); ctx.fillText('오류중학교 정보화 기기 수리 요청 대장', 10, 10); </script>")
