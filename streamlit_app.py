92eefd105c2eaab613092e63f710bd42ada33445
shimverz-app <shimverz@gmail.com>
2025-01-02 14:38:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,2 @@
 import streamlit as st
-from streamlit.components.v1 import html
-st.html("<svg><text>오류중학교 정보화 기기 수리 요청 대장</text></svg>")
+st.html("<svg width=700px height=100px><text>오류중학교 정보화 기기 수리 요청 대장</text></svg>")
