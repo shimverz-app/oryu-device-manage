82e701ca738ebd1c871829d405b363f9dd3d8acd
shimverz-app <shimverz@gmail.com>
2025-01-02 21:32:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def change_label_style(label, font_size='12px', font_color='black', font_family=
 st.markdown("<span style='font-size: 23px;'>오류중학교 정보화 기기 수리 요청 대장</span>", unsafe_allow_html=True)
 
 
-colms = st.columns((1, 1, 2, 2, 1, 1, 2))
+colms = st.columns((1, 1, 1, 1, 1, 1, 1))
 fields = ['만든 날짜', '수리 요청인', '수리 대상물 위치(부서/교실)', '수리 점검 물품(ex.컴퓨터, 노트북)', '고장 상태', '처리 상태', '비고']
 for col, field_name in zip(colms, fields):
   col.write(field_name)
