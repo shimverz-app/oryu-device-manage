24429c9b4bb66ee6775b0f3fd2ffabe003d8e1da
shimverz-app <shimverz@gmail.com>
2025-01-02 21:13:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ st.dataframe(pd.DataFrame([['gg','hh'],['zz','fv']], columns=['접수 일시', '
 if st.checkbox('show/hide'):
   st.text('뭔가 하겠다.')
   st.text(dt.datetime.now())
+
+lang = ['python', 'Java']
+select_lang = st.selectbox('언어를 선택하세요.', lang)
+st.write('당신이 선택한 언어는 {}입니다.',format(select_lang))
