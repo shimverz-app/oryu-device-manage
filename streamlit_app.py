291f1b814542aba86605ecd60d47a33f4390710c
shimverz-app <shimverz@gmail.com>
2025-01-02 21:08:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import pandas as pd
 
 st.subheader("오류중학교 정보화 기기 수리 요청 대장")
 st.dataframe(pd.DataFrame([['gg','hh'],['zz','fv']], columns=['접수 일시', '수리 요청자']))
+
+if st.checkbox('show/hide'):
+  st.text('뭔가 하겠다.')
+
