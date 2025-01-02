dd7aa9339bb171f4e562d163bc5b8414dea7839b
shimverz-app <shimverz@gmail.com>
2025-01-02 14:37:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 from streamlit.components.v1 import html
-st.markdown("<canvas style='width: 700px; height: 100px' id='myCanvas'>안녕하세여.</canvas><p>안녕하세요.</p> <script> const canvas = document.getElementById('myCanvas'); const ctx = canvas.getContext('2d'); ctx.fillText('오류중학교 정보화 기기 수리 요청 대장', 10, 10); </script>", unsafe_allow_html=True)
+st.html("<svg><text>오류중학교 정보화 기기 수리 요청 대장</text></svg>")
