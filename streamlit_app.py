d87e6bc6f56b9f459a8020b71b25f4ccb410d73a
shimverz-app <shimverz@gmail.com>
2025-01-02 21:16:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ lang = ['python', 'Java']
 select_lang = st.selectbox('언어를 선택하세요.', lang)
 st.write('당신이 선택한 언어는 {}입니다.',format(select_lang))
 
-input = st.text_input("message")
+input = st.text_input("message", max_chars=5)
 st.write(input)
