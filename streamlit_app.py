be7e9ea2f202188132525cc97213180f8f65235c
shimverz-app <shimverz@gmail.com>
2025-01-02 21:06:34 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-st.markdown("오류중학교 정보화 기기 수리 요청 대장")
+st.subtitle("오류중학교 정보화 기기 수리 요청 대장")
 st.dataframe(pd.DataFrame([['gg','hh'],['zz','fv']], columns=['접수 일시', '수리 요청자']))
