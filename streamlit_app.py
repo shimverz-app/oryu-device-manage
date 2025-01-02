c4db21c8cf57451beeb0054dc24086c3b7520a29
shimverz-app <shimverz@gmail.com>
2025-01-02 21:25:00 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,9 @@ import pandas as pd
 import datetime as dt
 
 st.subheader("오류중학교 정보화 기기 수리 요청 대장")
-st.dataframe(pd.DataFrame([['gg','hh'],['zz','fv']], columns=['접수 일시', '수리 요청자']))
 
-if st.checkbox('show/hide'):
-  st.text('뭔가 하겠다.')
-  st.text(dt.datetime.now())
 
-lang = ['python', 'Java']
-select_lang = st.selectbox('언어를 선택하세요.', lang)
-st.write('당신이 선택한 언어는 {}입니다.',format(select_lang))
-
-input = st.text_input("message", max_chars=5)
-st.write(input)
+colms = st.columns((1, 1, 1, 1, 2, 1,2))
+fields = ['만든 날짜', '수리 요청인', '수리 대상물 위치(부서/교실)', '수리 점검 물품(ex.컴퓨터, 노트북)', '고장 상태', '처리 상태', '비고']
+for col, field_name in zip(colms, fields):
+  col.write(field_name)
