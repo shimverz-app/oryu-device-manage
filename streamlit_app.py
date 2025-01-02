686b70505ad4fb26ade9b7b926f560708d05ceea
shimverz-app <shimverz@gmail.com>
2025-01-02 22:13:39 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import datetime as dt
 from st_aggrid import AgGrid, GridOptionsBuilder
 
 st.markdown("<span style='font-size: 23px;'>오류중학교 정보화 기기 수리 요청 대장</span>", unsafe_allow_html=True)
+data = pd.DataFrame([{'make_date': '11'}])
 
 gb = GridOptionsBuilder()
 gb.configure_default_column(
@@ -14,4 +15,4 @@ gb.configure_default_column(
 )
 gb.configure_column(field="make_date", header_name="요청 날짜", width=80)
 go = gb.build()
-grid_return = AgGrid([{'make_date': '11'}], gridOptions=go)
+grid_return = AgGrid(data, gridOptions=go)
