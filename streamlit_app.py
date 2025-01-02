86678562f7db42cf6cbff1f6742e77a0e23020a6
shimverz-app <shimverz@gmail.com>
2025-01-02 21:27:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,18 @@ import streamlit as st
 import pandas as pd
 import datetime as dt
 
+def change_label_style(label, font_size='12px', font_color='black', font_family='sans-serif'):
+    html = f"""
+    <script>
+        var elems = window.parent.document.querySelectorAll('p');
+        var elem = Array.from(elems).find(x => x.innerText == '{label}');
+        elem.style.fontSize = '{font_size}';
+        elem.style.color = '{font_color}';
+        elem.style.fontFamily = '{font_family}';
+    </script>
+    """
+    st.components.v1.html(html)
+
 st.subheader("오류중학교 정보화 기기 수리 요청 대장")
 
 
@@ -9,3 +21,4 @@ colms = st.columns((2, 2, 4, 4, 2, 1,2))
 fields = ['만든 날짜', '수리 요청인', '수리 대상물 위치(부서/교실)', '수리 점검 물품(ex.컴퓨터, 노트북)', '고장 상태', '처리 상태', '비고']
 for col, field_name in zip(colms, fields):
   col.write(field_name)
+  change_label_style(fields, '10px')
