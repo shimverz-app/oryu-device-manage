8ceb4e55e744d6d1673d3d45d8264f45c36c112f
shimverz-app <shimverz@gmail.com>
2025-01-02 22:52:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,13 @@ gb = GridOptionsBuilder()
 gb.configure_default_column(
     resizable=False
 )
-gb.configure_column(field="make_date", header_name="요청 날짜", editable=True, width=200)
+gb.configure_column(field="make_date", header_name="요청 날짜", editable=True, width=100)
 gb.configure_column(field="people", header_name="수리 요청인", width=80)
 gb.configure_column(field="place", header_name="수리 대상물 위치(부서/교실)", width=80)
 gb.configure_column(field="device", header_name="수리 점검 물품(ex.컴퓨터, 노트북)", width=80)
 gb.configure_column(field="happen", header_name="고장 상태", width=80)
-gb.configure_column(field="tf", header_name="처리 상태", width=80)
-gb.configure_column(field="noop", header_name="비고", width=80)
+gb.configure_column(field="tf", header_name="처리 상태", width=80, editable=True)
+gb.configure_column(field="noop", header_name="비고", width=80, editable=True)
 go = gb.build()
 
 AgGrid(data, gridOptions=go)
