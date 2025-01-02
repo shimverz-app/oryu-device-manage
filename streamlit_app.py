dd31660f302dab13e30b475d4a7c4f9995d4179b
shimverz-app <shimverz@gmail.com>
2025-01-02 14:20:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,4 @@ import streamlit as st
 
 st.html("<canvas id='myCanvas' width='100%'></canvas>")
 
-st.html("const canvas = document.getElementById('myCanvas');
-        const ctx = canvas.getContext('2d');
-        ctx.fillText('오류중학교 정보화 기기 수리 요청 대장', 10, 10);")
+st.html("const canvas = document.getElementById('myCanvas'); const ctx = canvas.getContext('2d'); ctx.fillText('오류중학교 정보화 기기 수리 요청 대장', 10, 10);")
