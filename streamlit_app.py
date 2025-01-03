ae53815dfed7d05d3eb7916d50b81e1fd40059dd
shimverz-app <shimverz@gmail.com>
2025-01-03 13:04:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ headers = {
     "Authorization": f"Bearer {access_token}"
 }
 
-r = requests.get('https://graph.microsoft.com/v1.0/groups/e49eee20-ef81-4f29-93aa-951daa958fca/sites/root/lists', headers=headers)
+r = requests.get('https://graph.microsoft.com/v1.0/groups/e49eee20-ef81-4f29-93aa-951daa958fca/sites/root/lists/7cbc9d8e-f383-46c8-a205-22ac00f3d842', headers=headers)
 
 st.write(r.text)
 
